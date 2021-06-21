fa63702042a981c825a80dd15d3be29ca53b3a79
Active Fog <email@sjc.sh>
2021-06-21 03:00:23 -0500

    Re-insert navbar to all pages

---
+++ blog/.vuepress/components/BlogHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>{{ $page.frontmatter.title }}</h1>
+    <p>{{ $page.frontmatter.date }}</p>
+    <p>by {{ $page.frontmatter.author }}</p>
+  </div>
+</template>
--- blog/.vuepress/theme/layouts/Post.vue
+++ blog/.vuepress/theme/layouts/Post.vue
@@ -1,3 +1,3 @@
 <template>
- <IndexPost />
+  <Layout />
 </template>
