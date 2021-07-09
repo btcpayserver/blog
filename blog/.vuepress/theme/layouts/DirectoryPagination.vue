fced252164ed41b896bbae9ba9a0dc65e3d78fb9
Dennis Reimann <mail@dennisreimann.de>
2021-07-09 09:34:57 +0200

    Add IndexPost reference

--- blog/.vuepress/theme/layouts/DirectoryPagination.vue
+++ blog/.vuepress/theme/layouts/DirectoryPagination.vue
@@ -1,3 +1,13 @@
 <template>
- <IndexPost />
+  <IndexPost />
 </template>
+
+<script>
+import IndexPost from './IndexPost.vue'
+
+export default {
+  components: {
+    IndexPost
+  },
+}
+</script>
